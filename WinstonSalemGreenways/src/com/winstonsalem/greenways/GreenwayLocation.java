24f6fd3f28cf85151b74f3ffc5f7671a62f17781
Priyanka Jalisatgi <priyankagj@gmail.com>
2013-03-19 23:13:20 -0400

    greenway class n location class

---
+++ WinstonSalemGreenways/src/com/winstonsalem/greenways/GreenwayLocation.java
@@ -0,0 +1,21 @@
+package com.winstonsalem.greenways;
+
+public class GreenwayLocation {
+	int latitude;
+	int longitude;
+	
+	public int getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(int latitude) {
+		this.latitude = latitude;
+	}
+	public int getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(int longitude) {
+		this.longitude = longitude;
+	}
+	
+	
+}
